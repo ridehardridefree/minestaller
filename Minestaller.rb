fac877e342935fed661d0abb65d5cedfc4f2e25e
Mark Roberts <ridehardridefree.mr@gmail.com>
2023-12-19 22:53:45 -0800

    Added header information.

---
+++ Minestaller.rb
@@ -0,0 +1,2 @@
+#Minestaller | Version 1 | Written by: Mark Roberts | License: Odds are it will be BSD
+#About: Minestaller is to be able to quickly create and update a Minecraft Bedrock server on Linux.
