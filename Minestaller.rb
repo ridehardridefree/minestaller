0f18b426bdffbb4eb9f4e0636965c649b93c8fdd
Mark Roberts <ridehardridefree.mr@gmail.com>
2024-01-30 18:13:53 -0800

    Added requierment for the Thor gem.  Added the containing class for Minestaller.

--- Minestaller.rb
+++ Minestaller.rb
@@ -1,2 +1,8 @@
 #Minestaller | Version 1 | Written by: Mark Roberts | License: Odds are it will be BSD
 #About: Minestaller is to be able to quickly create and update a Minecraft Bedrock server on Linux.
+
+require 'thor'
+
+class Minestaller < Thor
+
+end
